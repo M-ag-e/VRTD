ee6794cb49effb5cee181e338736cf91ba4e5795
Mage <georgeclark122@gmail.com>
2023-05-25 06:17:03 +0100

    TowerSpawning

---
+++ Assets/VRTD/Towers/TowerTile.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerTile : MonoBehaviour
+{
+    public BoxCollider boxTrigger;
+    public GameObject towerSpawnPoint;
+
+    public GameObject towerBasicPrefab;
+    public GameObject towerHeavyPrefab;
+    public GameObject towerAoEPrefab;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<TowerSpawnerData>()) 
+        {
+            switch (other.gameObject.GetComponent<TowerSpawnerData>().spawnerType)
+            {
+                case TowerSpawnerData.TowerSpawnerType.Basic:
+                    Instantiate(towerBasicPrefab,towerSpawnPoint.transform);
+                    break;
+
+                case TowerSpawnerData.TowerSpawnerType.Heavy:
+                    Instantiate(towerHeavyPrefab, towerSpawnPoint.transform);
+                    break;
+
+                case TowerSpawnerData.TowerSpawnerType.AoE:
+                    Instantiate(towerAoEPrefab, towerSpawnPoint.transform);
+                    break;
+
+            }
+            Destroy(other.gameObject);
+        }
+    }
+}
